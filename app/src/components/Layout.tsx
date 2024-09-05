e3e35a77fabab41aa57320afd9ecebe6c958ad9c
SolidityDrone <pnn.claudoio@gmail.com>
2024-09-05 17:39:17 +0200

    .

--- app/src/components/Layout.tsx
+++ app/src/components/Layout.tsx
@@ -5,11 +5,6 @@ export function Layout(props: PropsWithChildren) {
   return (
    
     <div className='flex flex-col bg-tv w-full min-h-screen'>
-  
- 
-
- 
-   
       <main className='flex-grow pt-10 pb-4 w-full'>{props.children}</main>
    
     </div>
